715055bf6c4d393098693b4821a1db5867bddd0a
dlulanep <74797289+dlulanep@users.noreply.github.com>
2025-03-26 22:03:53 +0200

    Add files via upload

---
+++ Assignment 3.sql
@@ -0,0 +1,11 @@
+CREATE database Student;
+USE student;
+CREATE table Student_no;
+
+CREATE table Student_name;
+
+CREATE table course
+INSERT COL(
+name (100);
+age int(2)
+gender varchar(String, )
